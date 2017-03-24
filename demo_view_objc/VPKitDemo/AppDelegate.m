ba023e544b6a9edcb7d30e87dc0f8100baccae38
foundry <foundry.tv@gmail.com>
2017-03-24 22:07:31 +0000

    deployed - v 2.4.26

--- demo_view_objc/VPKitDemo/AppDelegate.m
+++ demo_view_objc/VPKitDemo/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import <VPKit/VPKit.h>
+#import "VPKit/VPKit.h"
 @import AdSupport;
 
 
@@ -22,7 +22,8 @@
     
     /*
      
-     VPKIT CREDENTIALS
+     APP CREDENTIALS: required
+     
      These app credentials are for testing purposes only.
      To obtain credentials unique to your app email sdk_support@veepio.com
      
@@ -31,26 +32,6 @@
     NSString* appId = @"VEEPIO_test_app_id";
     NSString* clientId = @"VsRIkxIfTtkFJhw1ABItnO50B6fSW23NhIRnST53";
     NSString* clientSecret = @"OdWbCaP9i1I2AV2yZUzwfDFE4gU04RDX1HdubnTEg8oWw8F9yWQwjX179zHRXLUad5vrsOo5B7UtFq2utsrWbkjVus5aJKxW8wXTvDknqdgeowunL9yeEN8selNpTOJF";
-    NSString* userEmail = @"anoymous@example.com";
-    
-    /* 
-     
-     IDFA support
-    
-     (optional) send IDFA for Veep tracking
-     
-     This requires host app linking to AdSupport.framework ("link binary with libarires" section of project Build Phases)
-     
-     Setting this option to YES entails additional reporting requirements when submitting to the app store
-     
-      https://developer.apple.com/library/content/documentation/LanguagesUtilities/Conceptual/iTunesConnect_Guide/Chapters/SubmittingTheApp.html#//apple_ref/doc/uid/TP40011225-CH33-SW8
-     
-     */
-    
-    
-    [VPKit sendIDFA:YES];
-    
-
     
     /*
      
@@ -62,20 +43,35 @@
                    clientId:clientId
                clientSecret:clientSecret];
     
-    [VPKit setEmail:userEmail];
-    
-    
     /*
      
      SANDBOXING
-     Testing and development - NO
-     Deployment - YES
+     Different environments and databases are available for testing ("sandbox" and deployment ("production").
+     
+     Testing and development - set to NO (default if unset)
+     Deployment - set to YES
      
      */
     
     [VPKit setProduction:NO];
     
     
+    /*
+     
+     IDFA support - optional
+     
+     (optional) send IDFA for Veep tracking
+     
+     This requires host app linking to AdSupport.framework ("link binary with libarires" section of project Build Phases)
+     
+     Setting this option to YES entails additional reporting requirements when submitting to the app store
+     
+     https://developer.apple.com/library/content/documentation/LanguagesUtilities/Conceptual/iTunesConnect_Guide/Chapters/SubmittingTheApp.html#//apple_ref/doc/uid/TP40011225-CH33-SW8
+     
+     */
+    
+    
+    [VPKit sendIDFA:YES];
     
     /*
      
@@ -88,7 +84,6 @@
     VPKit.styles.color.navBar = [UIColor colorWithWhite:0.1 alpha:1.0];
     VPKit.styles.font.navBarFont = [UIFont systemFontOfSize:18 weight:UIFontWeightHeavy];
     VPKit.styles.font.cellNavBarFont = [UIFont systemFontOfSize:14 weight:UIFontWeightBold];
-    
     
     
     return YES;
