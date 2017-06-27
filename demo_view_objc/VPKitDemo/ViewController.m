3ec92e0e22f2b66ccbef76732b1e503df7ea46f4
foundry <foundry.tv@gmail.com>
2017-06-27 19:30:15 +0100

    fastlane:

--- demo_view_objc/VPKitDemo/ViewController.m
+++ demo_view_objc/VPKitDemo/ViewController.m
@@ -37,7 +37,8 @@
     [self configureViewer1];
     [self configureViewer2];
     [self configureConstraints];
-    
+    [self getstats];
+
 }
 
 
@@ -59,7 +60,14 @@
     self.viewerPreview2.image = image;
 }
 
-
+- (void)getstats {
+ 
+    
+    [VPKit getDailyStats:^(VPKDailyStats * _Nullable stats, NSError * _Nullable error) {
+        NSLog(@"getDailyStats %@ %@",stats,error);
+        
+    }];
+}
 
 #pragma mark - (layout boilerplate for the demo app, not relevant to the SDK)
 
