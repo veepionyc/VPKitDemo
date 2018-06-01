a1f15ac4d23a69961da65d9157d8d834017fcbbd
foundry <foundry.tv@gmail.com>
2018-06-01 01:47:18 +0100

    updated docs

---
+++ VPKit.framework/Headers/VPKPublicConstants.h
@@ -0,0 +1,19 @@
+//
+//  Header.h
+//  VPKit
+//
+//  Created by jonathan on 09/05/2018.
+//  Copyright © 2018 jonathan. All rights reserved.
+//
+
+#ifndef VPKPublicConstants_h
+#define VPKPublicConstants_h
+
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT NSString* const vpkErrorNotification;
+FOUNDATION_EXPORT NSString* const vpkErrorKey;
+FOUNDATION_EXPORT NSString* const vpkPresentErrorKey;
+
+#endif /* Header_h */
