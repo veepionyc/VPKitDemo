5cf00c825c715be6c7d066f32783e0c068cf70ca
foundry <foundry.tv@gmail.com>
2019-03-09 22:28:04 +0000

    VPKWindow added file

---
+++ VPKit.framework/Headers/VPKWindow.h
@@ -0,0 +1,17 @@
+//
+//  VPKWindow.h
+//  VPKUtils
+//
+//  Created by jonathan on 09/03/2019.
+//  Copyright © 2019 jonathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VPKWindow : UIWindow
+
+@end
+
+NS_ASSUME_NONNULL_END
