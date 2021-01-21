c5a11981f81395803d98bebe872102cae2c0f304
foundry <foundry.tv@gmail.com>
2021-01-21 00:08:18 +0000

    deployed - v 2.9.8

--- VPKit.framework/Headers/VPKAdHandler.h
+++ VPKit.framework/Headers/VPKAdHandler.h
@@ -11,24 +11,36 @@
 
 @import UIKit;
 /**
-Protocol for handling pre-roll video advertising
+Protocol for handling pre-roll video advertising.
+
+ When an object conforming to this protocol is assigned to a `VPKPreview` or `VPKVeepViewer`, VPKit video content will be loaded and paused to allow the host app to manage preroll advertising.
+ 
+ `view`, `controller` and `completion` objects will be returned to facilitate correct handling and display of prerolls.
+ 
+ When the `completion` block is called, control will return to the SDK to play the requested video content.
+ 
+ Designed for [GoogleAds Interactive Media Ads](https://developers.google.com/interactive-media-ads/docs/sdks/ios/client-side/) requirements.
  */
+
+
+
 @protocol VPKAdHandler
 
-/**
- Implement this method to provide view, controller and completion block to an advertising handler
 
+/**
+ Implement this method to provide view, controller and completion block to an advertising handler.
+ 
  @param     view          The view on top of which an advertising videoPlayer should be placed, using the same frame.
  @param     controller    The parent viewController for the `view`. This is the viewController that must be used
-                          to present an in-app browser for click-through ad content
- @param     completion    the completion function that must be called when ad handler is finished
+                          to present an in-app browser for click-through ad content.
+ @param     completion    The completion block that must be called when the preroll ad has finished playing. The VPKit SDK video will not play until this block is called.
 
  */
-
+ 
 - (void)handlePrerollOnView:(UIView*)view viewController:(UIViewController*)controller completion:(void(^)(void))completion;
 
 /**
- Implement this method to clean up interrupted ad handling
+ Implement this method to clean up interrupted ad handling.
  */
 
 - (void)stopPreroll;
